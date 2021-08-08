8fa38266b2c01fbbc2066a07d2bd6dbf329712de
francis mwas <mwangif184@gmail.com>
2021-08-08 12:51:52 +0300

    Added color box

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
 import Pallete from './Pallete';
 import seedColor from './seedColor';
 
 function App() {
   return (
-    <div className="App">
-      <Pallete {...seedColor[4]} />
+    <div>
+      <Pallete {...seedColor[2]} />
     </div>
   );
 }
 
-export default App; 
+export default App;
--- src/Pallete.js
+++ src/Pallete.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Pallete.css';
 
 class Pallete extends Component {
   render() {
+    const colorBoxes = this.props.colors.map((color) => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
     return (
       <div className="Pallete">
         {/* Navbar goes here  */}
-        <div className="Pallete-colors"></div>
+        <div className="Pallete-colors">{colorBoxes}</div>
         {/* footer  */}
       </div>
     );
