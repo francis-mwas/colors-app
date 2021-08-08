74d4786b98403da8d101afbbaf48c875d259fca3
francis mwas <mwangif184@gmail.com>
2021-08-08 11:59:12 +0300

    Application setup

---
+++ src/Pallete.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Pallete extends Component {
+  render() {
+    return (
+      <div className="Pallete">
+        {/* Navbar goes here  */}
+        <div className="Pallete-colors"></div>
+        {/* footer  */}
+      </div>
+    );
+  }
+}
+
+export default Pallete;
